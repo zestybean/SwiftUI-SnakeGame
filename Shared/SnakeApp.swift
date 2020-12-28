f7142d05e2c45c3884cb7e31432579880840f4bc
Brando Lugo <brando.lugo@gmail.com>
2020-12-28 17:53:33 -0600

    Initial Commit

---
+++ Shared/SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
